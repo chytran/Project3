22952c971ea398dbfac33d6a51c515ae72b457ef
chytran <chytran@users.noreply.github.com>
2021-08-30 16:00:44 -0700

    Set up

--- index.php
+++ index.php
@@ -10,6 +10,47 @@
     <link rel="stylesheet" href="css/style.css?v=<?php echo time(); ?>">
 </head>
 <body>
-    
+    <header class="header" id="header">
+        <nav class="nav container">
+            <a href="#" class="nav__logo">Portfolio</a>
+
+            <div class="nav__menu" id="nav-menu">
+                <ul class="nav__list">
+                    <li class="nav__item">
+                        <a href="#" class="nav__link">Home</a>
+                    </li>
+                    <li class="nav__item">
+                        <a href="#" class="nav__link">Home</a>
+                    </li>
+                    <li class="nav__item">
+                        <a href="#" class="nav__link">Home</a>
+                    </li>
+                    <li class="nav__item">
+                        <a href="#" class="nav__link">Home</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+    <section class="container__main" id="container-main">
+        <div class="section container">
+            <!--=============== NAVBAR =================-->
+
+
+            <!--===============  =================-->
+
+
+            <!--=============== CSS =================-->
+
+
+            <!--=============== CSS =================-->
+
+
+            <!--=============== CSS =================-->
+
+
+            <!--=============== CSS =================-->
+        </div>
+    </section>
 </body>
 </html>
