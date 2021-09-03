199794720427f76d17025e0b3e8e581aed183124
chytran <chytran@users.noreply.github.com>
2021-09-03 12:33:05 -0700

    Setting up left and right side of sign up

--- signup.php
+++ signup.php
@@ -8,31 +8,39 @@
 </head>
 <body>
     <section class="hero" id="hero">
-        <form action="includes/signup.inc.php" class="form" id="form">
-            <h2 class="signup__title">Sign up</h2>
+        <div class="left__side">
+            <h1 class="left__header header__main">Welcome Back!</h1>
+            <p class="left__text">To keep connected with us please login with your info</p>
+            <a href="login.php" class="button">Sign in</a>
+        </div>
 
-            <div class="name__container">
-                <label for="name">Name:</label>
-                <input type="text" name="name" id="name" class="name__input">
-            </div>
+        <div class="right__side">
+            <form action="includes/signup.inc.php" method="post" class="form" id="form">
+                <h2 class="signup__title">Create Account</h2>
 
-            <div class="email__container">
-                <label for="email">Email:</label>
-                <input type="email" name="email" id="email" class="email__input">
-            </div>
+                <div class="name__container">
+                    <label for="name">Name:</label>
+                    <input type="text" name="name" id="name" class="name__input">
+                </div>
 
-            <div class="password__container">
-                <label for="password">Password:</label>
-                <input type="password" name="password" id="password" class="password__input">
-            </div>
+                <div class="email__container">
+                    <label for="email">Email:</label>
+                    <input type="email" name="email" id="email" class="email__input">
+                </div>
 
-            <div class="passRepeat__container">
-                <label for="passRepeat">Name</label>
-                <input type="text" name="passRepeat" id="passRepeat" class="passRepeat__input">
-            </div>
+                <div class="password__container">
+                    <label for="password">Password:</label>
+                    <input type="password" name="password" id="password" class="password__input">
+                </div>
 
-            <input type="submit" class="button">
-        </form>
+                <div class="passRepeat__container">
+                    <label for="passRepeat">Password Confirmation:</label>
+                    <input type="text" name="passRepeat" id="passRepeat" class="passRepeat__input">
+                </div>
+
+                <input type="submit" class="button">
+            </form>
+        </div>
     </section>
 </body>
 </html>
