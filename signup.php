2c322e9682a480b4665d702a5a80a5137480ed10
chytran <chytran@users.noreply.github.com>
2021-09-04 11:58:08 -0700

    Updating sign up

--- signup.php
+++ signup.php
@@ -1,43 +1,43 @@
 <?php 
     include_once 'components/header.php';
 ?>
+<div class="main__container flex__column" id="main-container">
+    <section class="hero flex__row signup__container" id="hero">
+        <div class="left__side">
+            <div class="message__container flex__column">
+                <img class="bg" src="image/signup.jpg" alt="">
+                <h1 class="left__header header__main">Welcome Back!</h1>
+                <p class="left__text">To keep connected with us please login with your info</p>
+                <a href="login.php" class="button">Sign in</a>
+            </div>
+        </div>
 
-<section class="hero" id="hero">
-    <div class="left__side">
-        <h1 class="left__header header__main">Welcome Back!</h1>
-        <p class="left__text">To keep connected with us please login with your info</p>
-        <a href="login.php" class="button">Sign in</a>
-    </div>
-
-    <div class="right__side">
-        <form action="includes/signup.inc.php" method="post" class="form" id="form">
+        <div class="right__side flex__column">
             <h2 class="signup__title">Create Account</h2>
+            <form action="includes/signup.inc.php" method="post" class="form" id="form">
 
-            <div class="name__container">
-                <label for="name">Name:</label>
-                <input type="text" name="name" id="name" class="name__input">
-            </div>
+                <div class="name__container">
+                    <input type="text" name="name" id="name" class="name__input">
+                </div>
 
-            <div class="email__container">
-                <label for="email">Email:</label>
-                <input type="email" name="email" id="email" class="email__input">
-            </div>
+                <div class="email__container">           
+                    <input type="email" name="email" id="email" class="email__input">
+                </div>
 
-            <div class="password__container">
-                <label for="password">Password:</label>
-                <input type="password" name="password" id="password" class="password__input">
-            </div>
+                <div class="password__container">
+                    <input type="password" name="password" id="password" class="password__input">
+                </div>
 
-            <div class="passRepeat__container">
-                <label for="passRepeat">Password Confirmation:</label>
-                <input type="text" name="passRepeat" id="passRepeat" class="passRepeat__input">
-            </div>
+                <div class="passRepeat__container">
+                    <label for="passRepeat">Password Confirmation:</label>
+                    <input type="text" name="passRepeat" id="passRepeat" class="passRepeat__input">
+                </div>
 
-            <input type="submit" class="button">
-        </form>
-    </div>
-</section>
-
+                <input type="submit" class="button">
+            </form>
+        </div>
+    </section>
+</div>
 <?php
     include_once 'components/footer.php';
 ?>
