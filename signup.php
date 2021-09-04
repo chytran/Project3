f88c139e23c310a8e98268e0150d2e243393b596
chytran <chytran@users.noreply.github.com>
2021-09-04 13:39:07 -0700

    Setting up right side continue

--- signup.php
+++ signup.php
@@ -2,7 +2,7 @@
     include_once 'components/header.php';
 ?>
 <div class="main__container flex__column" id="main-container">
-    <img class="bg-main" src="image/search-background.jpg" alt="">
+    <img class="bg-main" src="image/main3-signup.jpg" alt="">
     <section class="hero flex__row signup__container" id="hero">
         <div class="left__side flex-column">
             <div class="message__container flex__column">
@@ -17,22 +17,21 @@
 
         <div class="right__side flex__column">
             <h2 class="signup__title">Create Account</h2>
+            <p class="right__text">Please enter your information to sign up now!</p>
             <form action="includes/signup.inc.php" method="post" class="form flex__column" id="form">
 
                 <div class="name__container input__design">
                     <div class="input__extra__container">
-
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="18" height="18"><path fill="none" d="M0 0h24v24H0z"/><path d="M4 22a8 8 0 1 1 16 0h-2a6 6 0 1 0-12 0H4zm8-9c-3.315 0-6-2.685-6-6s2.685-6 6-6 6 2.685 6 6-2.685 6-6 6zm0-2c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4z"/></svg>
+                        <input type="text" name="name" id="name" placeholder="Name" class="name__input input__info">
                     </div>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="18" height="18"><path fill="none" d="M0 0h24v24H0z"/><path d="M4 22a8 8 0 1 1 16 0h-2a6 6 0 1 0-12 0H4zm8-9c-3.315 0-6-2.685-6-6s2.685-6 6-6 6 2.685 6 6-2.685 6-6 6zm0-2c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4z"/></svg>
-                    <input type="text" name="name" id="name" placeholder="Name" class="name__input input__info">
                 </div>
 
                 <div class="email__container input__design"> 
                     <div class="input__extra__container">
-                        
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="18" height="18"><path fill="none" d="M0 0h24v24H0z"/><path d="M3 3h18a1 1 0 0 1 1 1v16a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1zm17 4.238l-7.928 7.1L4 7.216V19h16V7.238zM4.511 5l7.55 6.662L19.502 5H4.511z"/></svg>
+                        <input type="email" name="email" id="email" placeholder="Email" class="email__input  input__info">
                     </div> 
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="18" height="18"><path fill="none" d="M0 0h24v24H0z"/><path d="M3 3h18a1 1 0 0 1 1 1v16a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1zm17 4.238l-7.928 7.1L4 7.216V19h16V7.238zM4.511 5l7.55 6.662L19.502 5H4.511z"/></svg>
-                    <input type="email" name="email" id="email" placeholder="Email" class="email__input  input__info">
                 </div>
 
                 <div class="password__container input__design">
