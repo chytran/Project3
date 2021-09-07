cf3ccf742e7ee6c9ca9ad3395562380aff74f8fe
chytran <chytran@users.noreply.github.com>
2021-09-07 14:12:36 -0700

    Web scraping in progress

--- python/main.py
+++ python/main.py
@@ -1,5 +1,20 @@
-import pandas as pd
+import requests
+from bs4 import BeautifulSoup
 
-df = pd.read_html('https://www.zillow.com/homes/San-Jose,-CA_rb/')
+url = 'https://www.zillow.com/homes/San-Jose,-CA_rb/'
 
-print(df)
+r = requests.get(url)
+soup = BeautifulSoup(r.content, 'html.parser')
+lists = soup.find_all('article', class_="list-card")
+
+for list in lists:
+    price = lists.find_all('a', class_="list-card-price")
+    address = lists.find_all('address', class_="list-card-addr")
+
+    # Bedroom
+    bedDiv = soup.find('div', {'class': 'list-card-details'})
+    bedNumber = bedDiv.find()
+
+    # Bathroom
+    price = lists.find_all('a', class_="list-card-price")
+    price = lists.find_all('a', class_="list-card-price")
