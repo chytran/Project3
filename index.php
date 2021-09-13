77aac9fa9eafc155e92141316cb18c2b95d49529
chytran <chytran@users.noreply.github.com>
2021-09-13 13:27:54 -0700

    Home page and Smoke animation

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 
 <header class="header" id="header">
     <nav class="nav container">
-        <a href="#" class="nav__logo">Travel</a>
+        <a href="#" class="nav__logo">Home</a>
 
         <div class="nav__menu" id="nav-menu">
             <ul class="nav__list">
@@ -18,7 +18,7 @@
                     <a href="#discover" class="nav__link">Discover</a>
                 </li>
                 <li class="nav__item">
-                    <a href="#place" class="nav__link">Place</a>
+                    <a href="login.php" class="nav__link">Login</a>
                 </li>
             </ul>
 
@@ -30,28 +30,80 @@
         </div>
     </nav>
 </header>
-<main class="main-1">
-    <section class="container__main" id="container-main">
-        <div class="section container">
-            <!--=============== NAVBAR =================-->
-            
 
-            <!--===============  =================-->
+<!--============= MAIN =============-->
+<main class="main">
+    <!--============= HOME =============-->
+    <section class="home" id="home">
+        <img src="image/search-background.jpg" alt="" class="home__img">
 
+        <div class="home__container container grid">
+            <div class="home__data">
+                <span class="home__data-subtitle">Discover your place</span>
+                <h1 class="home__data-title">Explore The <br> Best <br> Beautiful <br> Houses</h1>
+                <a href="#" class="button">Explore</a>
+            </div>
 
-            <!--=============== CSS =================-->
+            <div class="smoke" id="smoke">
+                <ul class="smoke1">
+                    <ul>
+                        <li>o</li>
+                        <li>s</li>
+                        <li>w</li>
+                        
+                    </ul>
+                    <ul>
+                        <li>o</li>
+                        <li>s</li>
+                        <li>t</li>
+                        <li>c</li>
+                        <li>c</li>
 
+                    </ul>
+                    <ul>
+                        <li>o</li>
+                        <li>s</li>
+                        <li>t</li>
+                        <li>c</li>
 
-            <!--=============== CSS =================-->
+                    </ul>
+                    <ul>
+                        <li>o</li>
+                        <li>s</li>
+                        <li>w</li>
+                    </ul>
+                </ul>
+            </div>
 
+            <div class="home__social">
+                <a href="https://facebook.com/" target="_blank" class="home__social-link">
+                    <i class="ri-facebook-box-fill"></i>
+                </a>
+                <a href="https://instagram.com/" target="_blank" class="home__social-link">
+                    <i class="ri-instagram-fill"></i>
+                </a>
+                <a href="https://twitter.com/" target="_blank" class="home__social-link">
+                    <i class="ri-twitter-fill"></i>
+                </a>
+            </div>
 
-            <!--=============== CSS =================-->
+            <div class="home__info">
+                <div>
+                    <span class="home__info-title">5 best houses to visit</span>
+                    <a href="#" class="button button--flex button--link home__info-button">
+                        More <i class="ri-arrow-right-line"></i>
+                    </a>
+                </div>
 
-
-            <!--=============== CSS =================-->
+                <div class="home__info-overlay">
+                    <img src="/assets/img/home2.jpg" alt="" class="home__info-img">
+                </div>
+            </div>
         </div>
     </section>
 </main>
+<script src="js/main.js"></script>
+
 <?php
     include_once 'components/footer.php';
 ?>
