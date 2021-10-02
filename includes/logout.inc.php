bdebf6ef33d903a0173e382e4df0fbb28d4a054f
chytran <chytran@users.noreply.github.com>
2021-10-02 14:18:45 -0700

    In progress

--- includes/logout.inc.php
+++ includes/logout.inc.php
@@ -1,11 +1,13 @@
 <?php
     session_start();
+    session_destroy();
+    // if(isset($_SESSION['user'])) {
+    //     session_destroy();
+    //     echo  "<script>location.href='../login.php'</script>";
+    //     // header("location: ../index.php");
+    // }
+    // else {
 
-    if(isset($_SESSION['user'])) {
-        session_destroy();
-        header("location: ../index.php");
-    }
-    else {
-        echo "There was an error to the logout";
-    }
+    //     echo "There was an error to the logout";
+    // }
 ?>
