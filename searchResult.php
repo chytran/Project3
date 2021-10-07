347e62d05896cdd32cd491f5a0c7524d38e0776d
chytran <chytran@users.noreply.github.com>
2021-10-06 20:34:09 -0700

    Testing search page

--- searchResult.php
+++ searchResult.php
@@ -64,6 +64,7 @@ if (isset($_POST['submit'])) {
     $result = mysqli_query($conn, $sql);
     $resultCheck = mysqli_num_rows($result);
 
+    
     if ($resultCheck > 0) {
         while ($row = mysqli_fetch_assoc($result)) {
             echo "<div class='house__info'>";
