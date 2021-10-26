07847b9d479acbde851648615e25bb7f75efa749
chytran <chytran@users.noreply.github.com>
2021-10-26 15:28:24 -0700

    detailed zipcode set up

--- searchResult.php
+++ searchResult.php
@@ -47,6 +47,12 @@
         <input type="text" name="search" class="search__result__text" placeholder="Enter a city, address, zipcode, property type or sqft...">
         <input type="submit" class="button search__button" name="submit">
     </form>
+    
+    <div class="zipCode__expand">
+        <input type="text" id="name"> 
+        <a href='javascript:setup()' id="import">Get Details</a>              
+    </div>
+
 
     <div id="map" class="map__container" style="height: 400px; width: 350px; position: relative; z-index: 100;">
         <!-- <iframe style="position: relative; z-index: 100;" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3172.370930560317!2d-121.89211318474916!3d37.3337261798421!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808fccbb4e751f57%3A0x7bb970c4219e6acd!2sSan%20Jos%C3%A9%20Museum%20of%20Art!5e0!3m2!1sen!2sus!4v1632683176648!5m2!1sen!2sus" width="350" height="400" style="border:0;" allowfullscreen="" loading="lazy"></iframe> -->
@@ -103,7 +109,14 @@ if (isset($_POST['submit'])) {
 
 <script src="js/search.js"></script>
 <script src="http://maps.googleapis.com/maps/api/js?key=AIzaSyA7IZt-36CgqSGDFK8pChUdQXFyKIhpMBY&sensor=true" type="text/javascript"></script>
-
+<script>
+    function setup() {
+        const message = document.getElementById("name").value;
+        // alert(`<a id="import" href="ms-excel:ofe|u|file:///C:/Users/kevin/OneDrive/Documents/excel/ACS Housing Summary, ${message}.xlsx">Open in Excel</a>`);
+        // console.log(message);
+        document.getElementById("import").innerHTML = `<a id="import" href="ms-excel:ofe|u|file:///C:/Users/kevin/OneDrive/Documents/excel/ACS Housing Summary, ${message}.xlsx">Open in Excel</a>`;
+    }
+</script>
 <?php
     include_once 'components/footer.php';
 ?>
