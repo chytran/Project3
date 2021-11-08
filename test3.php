28f740e7a6ebf3d06ac0e6138a26ea64b0704a0a
chytran <chytran@users.noreply.github.com>
2021-11-08 12:53:30 -0800

    testing continue

--- test3.php
+++ test3.php
@@ -1,14 +1,16 @@
 <?php
 
 header("Content-type: application/vnd.ms-excel");
-header("Content-Disposition: attachment; filename=chillyfacts.xls");
+header("Content-Disposition: attachment; filename=chillyfacts.csv");
 
 if (($handle = fopen("mapcode/zillowdata.csv", 'r')) !== false) {
     echo "<table style='width:100%' border='1'>";
-        while (($data = fgetcsv($handle, 1000, ",")) !== false) {
-            $num = count($data);
+        echo "<tr>";
+            echo "<th>latitude</th>";
+            echo "<th>longitude</th>";
+        echo "</tr>";
+        while (($data = fgetcsv($handle, 1000, ",")) !== false) {           
             // echo "<p> $num fields in line $row: <br /></p>\n";
-            $row++;
             if ($data[2] == "Corona") {
                 echo "<tr>";
                     echo "<th>" . $data[8] . "</th>";
