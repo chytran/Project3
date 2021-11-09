05c47bd8fe751696d6367c57321fdd428c285540
chytran <chytran@users.noreply.github.com>
2021-11-08 20:53:32 -0800

    test

--- test1.php
+++ test1.php
@@ -1,29 +1,32 @@
 <?php
-    $row = 1;
-    if (($handle = fopen("mapcode/zillowdata.csv", 'r')) !== false) {
-        while (($data = fgetcsv($handle, 1000, ",")) !== false) {
-            $num = count($data);
-            // echo "<p> $num fields in line $row: <br /></p>\n";
-            $row++;
-            if ($data[2] == "Corona") {
-                echo $data[8] . "<br />\n";
-                echo $data[9] . "<br />\n";
-                echo "<br />\n";
-            }      
-            else {
-                continue;
-            }
-        }
-        fclose($handle);
-    }
-    // $csv = fopen('mapcode/zillowdata.csv', 'r');
+    
+$headers = array("latitude", "longitude");
 
-    // while ($row = fgetcsv($csv)) {
-    //     if ($row[2] == "Van Nuys") {
-    //         echo implode(',', $row);
-    //         echo "\n";
+// array of data
+$dates = array(
+    array(
+        "latitude" => 1,
+        "longitude" => 1
+    )
+);
+
+if (($handle = fopen("mapcode/zillowdata.csv", 'r')) !== false) {
+    
+    // while (($data = fgetcsv($handle, 1000, ",")) !== false) {
+    //     if ($data[2] == "Corona") {
+    //         array_push($dates, $data[8], $data[9]);
     //     }
     // }
 
-    // fclose($csv);
-?>
+    $fh = fopen("coordinates.csv", "w");
+
+    // Creates the headers
+    fputcsv($fh, $headers);
+
+    // Populate the data
+    foreach($dates as $fields) {
+        fputcsv($fh, $fields);
+    }
+    fclose($handle);
+    fclose($fh);
+}
